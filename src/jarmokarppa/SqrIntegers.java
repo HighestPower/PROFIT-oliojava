1661773d542bb3d00aa7698eeaf733da1fa0ef58
Jarmo Kärppä <sontalaatikko@luukku.com>
2015-01-14 19:43:32 +0200

    Parit lämmittelytehtävät tehty.

--- src/jarmokarppa/SqrIntegers.java
+++ src/jarmokarppa/SqrIntegers.java
@@ -2,15 +2,14 @@ package jarmokarppa;
 
 public class SqrIntegers
 {
-	public static void main()
+	public static void main(String[] args)
 	{
 		for (int value = 0; value <= 1000; value++)
 		{
-			if ((Math.sqrt(value) % 2.0) == 0.0)
+			if ((Math.sqrt(value) % 1) == 0.0)
 			{
 			    System.out.println("Luku: " + value);
             }
 		}
 	}
-
 }
