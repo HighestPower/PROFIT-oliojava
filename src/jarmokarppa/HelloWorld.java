9a3ec44d8f873a671c2b0bbff21c4ed268744b97
Jarmo Kärppä <sontalaatikko@luukku.com>
2015-01-14 12:02:24 +0200

    Uudistetut rojut.

--- src/jarmokarppa/HelloWorld.java
+++ src/jarmokarppa/HelloWorld.java
@@ -3,7 +3,7 @@ package jarmokarppa;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Halloo siellä");
+        System.out.println("Halloo sinä siellä");
     }
 
 }
